ce9c5ca773df5e37985495ed121a5e73494803a5
Mikhail <mikhailyarkov2000@gmail.com>
2024-12-24 21:06:45 +0400

    fix bonus icon

--- src/pages/MainPage/components/DailyBonus/DailyBonusIcon/DailyBonusIcon.jsx
+++ src/pages/MainPage/components/DailyBonus/DailyBonusIcon/DailyBonusIcon.jsx
@@ -1,10 +1,19 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import styles from './dailyBonusIcon.module.scss'
 import { actionSetBonusWindowVisible } from '../../../../../state/reducers/dailyBonusReducer/dailyBonusReducer'
+import { useEffect, useState } from 'react'
 
 export const DailyBonusIcon = () => {
-    const isBonusGet = true
     const dispatch = useDispatch()
+    const bonuses = useSelector(state => state.dailyBonus.bonus.dailyBonuses || null)
+    const currentStreak = useSelector(state => state.dailyBonus.bonus.currentStreak || null)
+
+    const [isCanCollect, setIsCanCollect] = useState(false)
+    
+    useEffect(() => {
+        if (!bonuses) return
+        bonuses[currentStreak-1].isAvailable ? setIsCanCollect(true) : setIsCanCollect(false)
+    }, [bonuses])
 
     const handleCloseDailyWindow = () => {
         dispatch(actionSetBonusWindowVisible(true))        
@@ -12,7 +21,7 @@ export const DailyBonusIcon = () => {
     return(
         <div onClick={handleCloseDailyWindow} className={styles.container}>
             {
-                isBonusGet &&
+                isCanCollect &&
                 <div className={styles.container_marked}/>
             }
         </div>
