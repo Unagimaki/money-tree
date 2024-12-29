ad3a1cfdc5ceadaafc7364ddb6f6bc54af6954b3
Mikhail <mikhailyarkov2000@gmail.com>
2024-12-29 19:03:04 +0400

    fix limits

--- src/pages/StatsPage/components/WithdrawalBalance/WithdrawalBalance.jsx
+++ src/pages/StatsPage/components/WithdrawalBalance/WithdrawalBalance.jsx
@@ -11,7 +11,7 @@ export const WithdrawalBalance = () => {
     const currentCourse = useSelector(state => state?.season?.course)
     const balance = useSelector((state) => state.user.player.balance);
     const currentSum = balance * currentCourse
-    const daysLeft = useSelector(state => state.wallet.walletAdress?.daysLeft || null)
+    const daysLeft = useSelector(state => state.wallet.walletAdress?.daysLeft)
 
     const [isAvailable, setIsAvailable] = useState(false)
 
