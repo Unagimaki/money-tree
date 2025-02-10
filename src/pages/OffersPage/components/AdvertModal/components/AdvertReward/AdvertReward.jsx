d1bb181057756428eca7e7e01e9f9e22be051f7d
Mikhail <mikhailyarkov2000@gmail.com>
2025-02-10 21:56:42 +0400

    new layout

--- src/pages/OffersPage/components/AdvertModal/components/AdvertReward/AdvertReward.jsx
+++ src/pages/OffersPage/components/AdvertModal/components/AdvertReward/AdvertReward.jsx
@@ -6,12 +6,12 @@ export const AdvertReward = ({data}) => {
     return(
         <div className={styles.container}>
             <div className={styles.container_title}>Награды</div>
-            <AdvertRewardItemMain chance={data.chance[0].chance} place={data.chance[0].place} prize={data.chance[0].prize} />
+            <AdvertRewardItemMain chance={data.chance[0]?.chance} place={data.chance[0].place} prize={data.chance[0].prize} />
             <div className={styles.container_inner}>
                 {
                     data.chance.slice(1).map((item, index) => {
                         return <AdvertRewardItem
-                            chance={item.chance}
+                            chance={item?.chance}
                             prize={item.prize}
                             place={item.place}
                             key={index}
