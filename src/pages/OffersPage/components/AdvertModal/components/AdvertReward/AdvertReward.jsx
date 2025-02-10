bc8319766f0364a9861ae4a5441c3a3551e1c94c
Mikhail <mikhailyarkov2000@gmail.com>
2025-02-10 21:33:29 +0400

    new layout

--- src/pages/OffersPage/components/AdvertModal/components/AdvertReward/AdvertReward.jsx
+++ src/pages/OffersPage/components/AdvertModal/components/AdvertReward/AdvertReward.jsx
@@ -9,11 +9,12 @@ export const AdvertReward = ({data}) => {
             <AdvertRewardItemMain chance={data.chance[0].chance} place={data.chance[0].place} prize={data.chance[0].prize} />
             <div className={styles.container_inner}>
                 {
-                    data.chance.slice(1).map(item => {
+                    data.chance.slice(1).map((item, index) => {
                         return <AdvertRewardItem
                             chance={item.chance}
                             prize={item.prize}
                             place={item.place}
+                            key={index}
                         />
                     })
                 }
