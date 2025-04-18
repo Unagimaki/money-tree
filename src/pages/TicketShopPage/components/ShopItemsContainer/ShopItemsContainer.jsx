eb2423733479ff8c7c0f0891deae76253940f785
Dmitry <dmitrycrown@yandex.ru>
2025-04-18 19:48:54 +0300

    fix

--- src/pages/TicketShopPage/components/ShopItemsContainer/ShopItemsContainer.jsx
+++ src/pages/TicketShopPage/components/ShopItemsContainer/ShopItemsContainer.jsx
@@ -1,30 +1,29 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux'
 import styles from './shopItemsContainer.module.scss'
-import { ShopItem } from '../ShopItem/ShopItem';
+import { ShopItem } from '../ShopItem/ShopItem'
 
 export const ShopItemsContainer = ({ showModal, isModalVisible }) => {
-  const shopItems = useSelector(state => state.ticketShop)
-  const token = useSelector(state => state.user.token)
+	const shopItems = useSelector(state => state.ticketShop.items)
+	const token = useSelector(state => state.user.token)
+	console.log(shopItems)
 
-  const sortedItems = [...shopItems].sort((a, b) => a.tickets - b.tickets)
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.container_inner}>
-        {
-          sortedItems.map(item => (
-            <ShopItem
-              key={item.id}
-              id={item.id}
-              price={item.price}
-              tickets={item.tickets}
-              token={token}
-              showModal={showModal}
-              isModalVisible={isModalVisible}
-            />
-          ))
-        }
-      </div>
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<div className={styles.container_inner}>
+				{
+					shopItems.sort((a, b) => a.tickets - b.tickets).map(item => {
+						return <ShopItem
+							key={item.id}
+							id={item.id}
+							price={item.price}
+							tickets={item.tickets}
+							token={token}
+							showModal={showModal}
+							isModalVisible={isModalVisible}
+						/>
+					})
+				}
+			</div>
+		</div>
+	)
 }
